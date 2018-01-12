2f185e6854334863c3e01c4749f786a1266fb9d8
cowleyk <cowley.kevin.l@gmail.com>
2018-01-11 22:35:25 -0700

    :tada: split a list from a signal attribute into multiple signals

--- list_split_block.py
+++ list_split_block.py
@@ -1,12 +1,14 @@
 from nio.block.base import Block
-from nio.properties import VersionProperty
-
+from nio.properties import VersionProperty, Property, StringProperty
+from nio.block.base import Signal
 
 class ListSplit(Block):
 
     version = VersionProperty('0.1.0')
+    list_att = Property(title="List", default=[])
+    dict_att = StringProperty(title="Attribute Name", default="Attr")
 
     def process_signals(self, signals):
         for signal in signals:
-            pass
-        self.notify_signals(signals)
+            for elem in self.list_att(signal):
+                self.notify_signals([Signal({self.dict_att(signal): elem})])
